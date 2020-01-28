5795d5efc0357ad0d130cd6ee0272a2383f69cf0
jamievullo <jvullo@protonmail.com>
2020-01-28 10:05:45 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,22 @@
 // your Bomb code here!
+import React from 'react';
+
+export default class Bomb extends React.Component {
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+    
+    render () {
+        // return console.log('hello')
+        let message;
+            if(this.state.secondsLeft === 0) {
+                message = 'Boom!'
+            } else {
+                message = `${this.state.secondsLeft} seconds left before I go boom!`
+            }
+        return <div>{message}</div>
+    }
+}
